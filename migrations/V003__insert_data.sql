6084a34457cb574d5ce0173cd370b3a54170dffd
Denis Kornienko <korney.turk@gmail.com>
2025-03-08 19:35:09 +0300

    Add github secrets

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -7,3 +7,19 @@ INSERT INTO product (id, name, picture_url, price) VALUES
     (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00),
     (6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00)
 ON CONFLICT (id) DO NOTHING;
+
+
+-- Генерация заказов в orders
+INSERT INTO orders (id, status, date_created)
+SELECT i,
+       (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)],
+       DATE(NOW() - (random() * (NOW()+'90 days' - NOW())))
+FROM generate_series(1, 10000000) s(i)
+ON CONFLICT (id) DO NOTHING;
+
+-- Заполнение order_product (учитывая обновлённые связи)
+INSERT INTO order_product (quantity, order_id, product_id)
+SELECT floor(1 + random() * 50)::int, i, 1 + floor(random() * 6)::int
+FROM generate_series(1, 10000000) s(i)
+ON CONFLICT (order_id, product_id) DO NOTHING;
+
